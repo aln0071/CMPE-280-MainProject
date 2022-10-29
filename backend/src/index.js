d9a18b26a9e4c34767a2c045cd729ba82d66cd9b
Alan Kuriakose <aln0071@gmail.com>
2022-10-29 09:24:04 -0500

    feature/photo-upload: Get and put images to S3

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+const fs = require('fs')
+const util = require('util')
+const unlinkFile = util.promisify(fs.unlink)
+
+const { uploadFile, getFileStream } = require('./s3')
+
 
 const app = express();
 
@@ -10,4 +19,20 @@ app.get("/", (req, res) => {
     })
 })
 
-app.listen(port, () => console.log("[backend] listening on port "+port));
+// get image
+app.get('/image/:key', (req, res) => {
+    const key = req.params.key;
+    const readStream = getFileStream(key);
+    readStream.pipe(res);
+})
+
+// upload image
+app.post('/image', upload.single('image'), async (req, res, next) => {
+    const file = req.file;
+    const result = await uploadFile(file);
+    await unlinkFile(file.path)
+    // const description = req.body.description;
+    res.json(result);
+})
+
+app.listen(port, () => console.log("[backend] listening on port " + port));

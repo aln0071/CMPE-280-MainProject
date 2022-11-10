1b70b4d407761bfbde21a74e5b823a2ff30ef98b
Alan Kuriakose <aln0071@gmail.com>
2022-11-10 14:57:18 -0800

    [frontend][navbar]: remove unused nav link

--- frontend/src/Components/TopNavbar.jsx
+++ frontend/src/Components/TopNavbar.jsx
@@ -30,7 +30,6 @@ export default function TopNavbar({ navRef }) {
               <Nav.Link href="/#/home">Home</Nav.Link>
               {currentUser && <Nav.Link href="/#/createBlog">Create Blog</Nav.Link>}
               <Nav.Link href="/#/blogListing">Blog Listing</Nav.Link>
-              <Nav.Link href="/#/blog">Blog </Nav.Link>
             </Nav>
             {currentUser && (
               <Nav>
